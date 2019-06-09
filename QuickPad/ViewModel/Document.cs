83ec196fe787e14cb6c9f987fb7403babcedbd6b
Allan <allan.xiao@gmail.com>
2019-06-09 12:25:50 -0400

    Add project files.

---
+++ QuickPad/ViewModel/Document.cs
@@ -0,0 +1,54 @@
+﻿namespace QuickPad.ViewModel
+{
+    public class Document : Innouvous.Utils.Merged45.MVVM45.ViewModel
+    {
+        public bool HasChanges
+        {
+            get => Get<bool>();
+            private set
+            {
+                Set(value);
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        private string content;
+        public string Content {
+            get => content;
+            set
+            {
+                //TODO: Compare Last Saved Content
+                content = value;
+                RaisePropertyChanged();
+                HasChanges = true;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Get<string>() + (HasChanges ? "*" : "");
+            }
+            private set
+            {
+                Set(value);
+                RaisePropertyChanged("Header");
+            }
+        }
+
+        public Document(string name, string content = null)
+        {
+            Name = name;
+
+            HasChanges = false;
+
+            this.content = content;
+        }
+
+        public void Saved()
+        {
+            HasChanges = false;
+        }
+    }
+}
---
+++ QuickPad/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Innouvous.Utils.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuickPad.ViewModel
+{
+    public class MainWindowViewModel : Innouvous.Utils.Merged45.MVVM45.ViewModel
+    {
+        private ObservableCollection<Document> tabs = new ObservableCollection<Document>();
+        private Window window;
+
+        public Document CurrentTab
+        {
+            get
+            {
+                return Get<Document>();
+            }
+            set
+            {
+                Set(value);
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICollection<Document> Tabs
+        {
+            get
+            {
+                return tabs;
+            }
+        }
+
+        public MainWindowViewModel(Window window)
+        {
+            this.window = window;
+
+            AddDocument();
+            AddDocument();
+        }
+
+        private void AddDocument()
+        {
+            string title = "Document " + (Tabs.Count + 1);
+
+            var doc = new Document(title);
+            tabs.Add(doc);
+            CurrentTab = doc;
+        }
+
+        public ICommand SaveAllCommand
+        {
+            get
+            {
+                return new CommandHelper(SaveAll);
+            }
+        }
+
+        private void SaveAll()
+        {
+            foreach (var t in tabs)
+            {
+                t.Saved();
+            }
+        }
+    }
+}
